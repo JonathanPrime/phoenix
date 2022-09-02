0c8539db79b20b0a8cee92a3cf72c6a0b7409ff1
= <=>
2022-09-02 03:51:47 -0500

    f

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+'use strict';
+function popupWhatsApp() {
+  let btnClosePopup = document.querySelector('.closePopup');
+  let btnOpenPopup = document.querySelector('.whatsapp-button');
+  let popup = document.querySelector('.popup-whatsapp');
+  let sendBtn = document.getElementById('send-btn');
+
+  btnClosePopup.addEventListener("click", () => {
+    popup.classList.toggle('is-active-whatsapp-popup')
+  })
+
+  btnOpenPopup.addEventListener("click", () => {
+    popup.classList.toggle('is-active-whatsapp-popup')
+    popup.style.animation = "fadeIn .6s 0.0s both";
+  })
+
+  sendBtn.addEventListener("click", () => {
+    let msg = document.getElementById('whats-in').value;
+    let relmsg = msg.replace(/ /g, "%20");
+    //just change the numbers "1515551234567" for your number. Don't use +001-(555)1234567     
+    window.open('https://wa.me/573102305250?text=' + relmsg, '_blank');
+
+  });
+  /* Open pop-up in 15 seconds */
+  /* setTimeout(() => {
+    popup.classList.toggle('is-active-whatsapp-popup');
+  }, 15000); */
+}
